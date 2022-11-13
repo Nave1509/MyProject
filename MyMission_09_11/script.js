3fffb6dbdf9abda7d88f3231225ad0a342ea6a50
Nave <navecohen503@gmail.com>
2022-11-13 14:10:58 +0200

    Update Mission 09/11

--- MyMission_09_11/script.js
+++ MyMission_09_11/script.js
@@ -1,4 +1,4 @@
 import { GrandSon } from "./grandson.js";
 
-let newGrandson = new GrandSon("Cohen", "Google", "Tel-Aviv", "Hyundai");
+let newGrandson = new GrandSon("Cohen", "Google", "Jerusalem", "Hyundai");
 newGrandson.printAll();
